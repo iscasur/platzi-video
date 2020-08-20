e6fed378a33e1d9032ed7feb4da5674226cb602f
Israel Castro <israel.castro@gmail.com>
2020-08-20 12:16:00 -0500

    [Add] Search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Search.scss';
+
+const Search = () => (
+    <section className="main">
+        <h2 className="main__title">What do you want to see today?</h2>
+        <input className="input" type="text" placeholder="Buscar…" />
+    </section>
+);
+
+export default Search;
