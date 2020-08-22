46904f076b0fe4a1afe8d30fc7d9c4e3e88fbcd2
Israel Castro <israel.castro@gmail.com>
2020-08-22 13:27:19 -0500

    [Add] PropTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import "../assets/styles/components/CarouselItem.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../assets/styles/components/CarouselItem.scss';
 
-import playIcon from "../assets/static/play-icon.png";
-import plusIcon from "../assets/static/plus-icon.png";
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className="carousel-item">
@@ -27,5 +28,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/Header";
-import Search from "../components/Search";
-import Categories from "../components/Categories";
-import Carousel from "../components/Carousel";
-import CarouselItem from "../components/CarouselItem";
-import Footer from "../components/Footer";
-import useInitialState from "../hooks/useInitialState";
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
-import "../assets/styles/App.scss";
+import '../assets/styles/App.scss';
 
-const API = "http://localhost:3000/initialState";
+const API = 'http://localhost:3000/initialState';
 
 const App = () => {
   const initialState = useInitialState(API);
